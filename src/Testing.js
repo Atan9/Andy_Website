5913deaf8b2b3b23a1130e7e444b0b5279e047ef
atan9 <andyctan91@gmail.com>
2021-05-24 21:03:27 -0500

    Set up development for my first website

---
+++ src/Testing.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import logo from '.'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <img src= {logo2} className = "App-logo" alt = "logo"/>
+        <p>
+
+          yooo hello world!!!
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.google.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Google Link
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
